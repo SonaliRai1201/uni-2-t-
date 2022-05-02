aeb0bf82638c0df402a887d50d59b899fc119cb6
SonaliRai1201 <104216942+SonaliRai1201@users.noreply.github.com>
2022-05-02 16:42:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.querySelector("#masaiForm").addEventListener("submit",myfunction)
+    var arr=JSON.parse(localStorage.getItem("schedule"))||[];
+    function myfunction(){
+     event.preventDefault();
+
+     arrobj={
+       MatchNumber:masaiForm.matchNum,
+    TeamA:masaiForm.teamA.value,
+     TeamB:masaiForm.teamB.value,
+     date:masaiForm.date.value,
+     venue:masaiForm.venue.value,
+     }
+     arr.push(arrobj);
+    localStorage.setItem("schedule",JSON.stringify(arr))
+    window.location.href="matches.html"
+    }
